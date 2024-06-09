d633347d3d1a171ab8155f4fd8b021e7cfd582fb
Bamdad-Kr <149760246+Bamdad-Kr@users.noreply.github.com>
2024-06-09 22:04:38 +0330

    Update multiple-inheritance.py

--- multiple-inheritance.py
+++ multiple-inheritance.py
@@ -1,16 +1,16 @@
-class Flay:
-  def flay(self):
-    print("bird is flaying")
+class Fly:
+  def fly(self):
+    print("bird is flying")
 class Hunt:
   def hunt(self):
     print("bird need to hunt")
 class Land:
   def land(self):
-    print("bird  is landing")
-class Bird(Flay,Hunt,Land):
+    print("bird is landing")
+class Bird(Fly, Hunt, Land):
   pass
 
 b = Bird()
-print(b.flay())
+print(b.fly())
 print(b.hunt())
 print(b.land())
