e1b9136b8becd78501059294a8554a2716485d70
Bamdad-Kr <149760246+Bamdad-Kr@users.noreply.github.com>
2024-06-09 19:39:20 +0330

    Create function(include).py

---
+++ function(include).py
@@ -0,0 +1,14 @@
+def include 2, 5(i):
+  while i!=0:
+    k  = i%10
+    if k != 2 and k != 5:
+      return False
+    i//=10
+    return True
+
+def function2 (n):
+  for i in range (n):
+    k include 2, 5 = (i)
+    if k:
+      print(i)
+  return print("not find")
